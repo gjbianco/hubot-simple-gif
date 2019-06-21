22b20110d3e2f9ca1c27b6b99a94efac95dfd5f9
Guy Bianco IV <gjbiancoiv@gmail.com>
2019-06-21 11:04:32 -0400

    weight random index

--- src/hubot-simple-gif.js
+++ src/hubot-simple-gif.js
@@ -1,13 +1,22 @@
 var axios = require('axios')
 var apiKey = process.env.GIPHY_API_TOKEN
 
+function weightedRand(spec) {
+  const r = Math.floor(Math.random() * 100);
+  let sum = 0;
+  for (let i in spec) {
+    sum += spec[i];
+    if (r <= sum) return i;
+  }
+}
+
 module.exports = (robot) => {
 
   robot.hear(/^!gif (.*)$/i, (res) => {
     axios.get('http://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + res.match[1])
       .then((result) => {
         const gifList = result.data.data
-        const rand = Math.floor(Math.random() * 100 % 25)
+        const rand = weightedRand([0.5, 0.25, 0.12, 0.12, 0.01]) % 5;
         const randGif = gifList[rand]
 
         if (randGif) {
