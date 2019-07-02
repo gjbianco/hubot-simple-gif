afa70051687e27e847be965d21e8af99eb5e2de2
Guy Bianco IV <gjbiancoiv@gmail.com>
2019-07-02 14:42:30 -0400

    remove async-await

--- src/hubot-simple-gif.js
+++ src/hubot-simple-gif.js
@@ -34,8 +34,8 @@ function _processGifList(gifs) {
  * @param {string} query The giphy search string to query the service by.
  * @returns {Promise<string>} A Promise resolving to a string that should be sent to the channel.
  */
-async function getGif(query) {
-  await axios
+function getGif(query) {
+  return axios
     .get(`http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}`)
     .then(gifList => gifList.data.data)
     .then(_processGifList);
